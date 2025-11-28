041d36d988f4fca640e9ee373410731b96607b1d
Harshita Jujar <235818230+harshitakrishnan-5@users.noreply.github.com>
2025-11-28 11:28:52 +0530

    Update test_speed.py

--- test_speed.py
+++ test_speed.py
@@ -1,7 +1,7 @@
-# test_speed.py
 from speed import calculate_speed
 
 def test_calculate_speed():
     assert calculate_speed(100, 2) == 50
     assert calculate_speed(150, 3) == 50
     assert calculate_speed(60, 1) == 60
+
